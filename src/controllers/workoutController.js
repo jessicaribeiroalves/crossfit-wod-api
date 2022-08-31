bc4bc907ecf1e208c9435dc8520167379e5149e9
Jessica Alves <jessralves@gmail.com>
2022-08-31 11:50:38 -0400

    add remaining CRUD methods to controller

--- src/controllers/workoutController.js
+++ src/controllers/workoutController.js
@@ -6,8 +6,13 @@ const getAllWorkouts = (req, res) => {
 };
 
 const getOneWorkout = (req, res) => {
-    const workout = workoutService.getOneWorkout();
-    res.send("Get an existing workout");
+    const { params: { workoutId } } = req;
+    // we can improve the code below using a third party package (such as express-validator)
+    if (!workoutId) {
+        return;
+    }
+    const workout = workoutService.getOneWorkout(workoutId);
+    res.send({ status: 'OK', data: workout });
 };
 
 const createNewWorkout = (req, res) => {
@@ -34,13 +39,21 @@ const createNewWorkout = (req, res) => {
 };
 
 const updateOneWorkout = (req, res) => {
-    const updatedWorkout = workoutService.updateOneWorkout();
-    res.send("Update an existing workout");
+    const { body, params: { workoutId } } = req;
+    if (!workoutId) {
+        return;
+    }
+    const updatedWorkout = workoutService.updateOneWorkout(workoutId, body);
+    res.send({ status: 'OK', data: updatedWorkout });
 };
 
 const deleteOneWorkout = (req, res) => {
-    workoutService.deleteOneWorkout();
-    res.send("Delete an existing workout")
+    const { params: { workoutId } } = req;
+    if (!workoutId) {
+        return;
+    }
+    workoutService.deleteOneWorkout(workoutId);
+    es.status(204).send({ status: "OK" });
 }
 
 module.exports = {
